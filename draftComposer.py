951c504c8a277a1582a9626fa9ac4fee15f33d02
Amanda Bedard <bedard.amanda6@gmail.com>
2018-05-27 20:31:35 -0400

    Got functional essay composition, time to dress it up

--- draftComposer.py
+++ draftComposer.py
@@ -1,7 +1,10 @@
+#Amanda Bedard, 2018
 #File for composing the draft based on the type of essay
 from json import load
+from outlineReader import readDraft
 import random
 
+#Function to load essay 'fluff' json template
 def fluffGetter(fname):
     f = open(fname, 'r')
     data = load(f)
@@ -16,15 +19,40 @@ def draftChooser(essayType, draftData, fluffup=False):
         print("Sorry, that format is not currently supported")
 
 #Function to draft an opinion essay
-def opinionEssay(data, fluffup=False):
-    #Get the fluff for formatting the essay and prepare the essay string
-    FLUFF = 'essayFluff/opinion_transitions_1.json'
+def opinionEssay(data, fluffup=False, template='essayFluff/opinion_transitions_1.json'):
+    #Get the fluff for formatting the essay and initialize the essay string
+    print("Preparing your opinion essay, please wait..")
     essayStr = ''
     xtra = None
-    essayfluff = fluffGetter(FLUFF)
-    essayStr += introComposer("A BIG FANCY POINT", ["DETAIL 1", "DETAIL 2"], essayfluff['intro'])
+    essayfluff = fluffGetter(template)
+
+    #Get a few extra variables from the data
+    topic = data['title']
+    details = [k for k, v in data['data'].items()]
+
+    #Begin composing the essay, with the introduction
+    essayStr += firstlastComposer(topic, details, essayfluff['intro'])
+    
+    #Now, the body paragraphs
+    paragraphs = len(details) + 1
+    count = 1
+    for detail in details:
+        if count == 1:
+            essayStr += composebody("1", detail, data['data'][detail], essayfluff['topic'], essayfluff['details'])
+        elif count == paragraphs:
+            essayStr += composebody("3", detail, data['data'][detail], essayfluff['topic'], essayfluff['details'])
+        else:
+            essayStr += composebody("2", detail, data['data'][detail], essayfluff['topic'], essayfluff['details'])
+        count += 1
+
+    #Finally, the conclusion paragraph
+    essayStr += firstlastComposer(topic, details, essayfluff['conclusion'])
+
+    #Essay is done! Time to save.
+    print("Finished composing the essay, please wait..")
     saveEssay(essayStr)
 
+#Function to save the essay to a file location or default the current directory
 def saveEssay(essayStr, filelocation='./'):
     try:
         print("Saving file, please wait..")
@@ -34,13 +62,13 @@ def saveEssay(essayStr, filelocation='./'):
         print("Save Successful!")
     except:
         print("Save Failure.  Please try again.")
-    
 
-def introComposer(point, topics, fluff, xtra=None):
+#Function to compose an introductory or conclusion paragraph    
+def firstlastComposer(point, topics, fluff, xtra=None):
     intro = '    '
 
     rnd = random.randrange(0, len(fluff['1']))  
-    intro += fluff['1'][rnd] % point
+    intro += (fluff['1'][rnd] % point).capitalize().replace(' i ', ' I ')
 
     rnd = random.randrange(0, len(fluff['2']))
     topicstr = ''
@@ -49,18 +77,35 @@ def introComposer(point, topics, fluff, xtra=None):
             topicstr += ('and %s' % topic)
         else:
             topicstr += ('%s, ' % topic)
-    intro += fluff['2'][rnd] % topicstr
+    intro += (fluff['2'][rnd] % topicstr).capitalize().replace(' i ', ' I ')
 
     if xtra:
         rnd = random.randrange(0, len(xtra))
 
     rnd = random.randrange(0, len(fluff['3']))  
-    intro += fluff['3'][rnd]
+    intro += (fluff['3'][rnd]).capitalize().replace(' i ', ' I ')
     intro += '\n'
     return intro
 
-    
+def composebody(num, detail, substance, fluff, dfluff, xtra=None):
+    body = '    '
+    subtopics = [k for k, v in substance.items()]
+
+    #The transition statement
+    rnd = random.randrange(0, len(fluff[num]))  
+    body += (fluff[num][rnd] % detail).capitalize().replace(' i ', ' I ')
+
+    for topic in subtopics:
+        #Add the topics to the body.
+        rnd = random.randrange(0, len(dfluff))  
+        body += (dfluff[rnd] % topic).capitalize().replace(' i ', ' I ')
+        #Fill in the finer details
+        if substance[topic] != []:
+            for deet in substance[topic]:
+                body += '%s. ' % deet.capitalize().replace(' i ', ' I ')
+    #Newline at the end of the paragraph, then send it back!
+    body += '\n'
+    return body
 
 
-
-print(opinionEssay(None))
+opinionEssay(readDraft('outline.txt'))
--- outlineReader.py
+++ outlineReader.py
@@ -1,3 +1,4 @@
+#Amanda Bedard, 2018
 #File for reading and interpreting the outline
 
 #Function to read the draft from a file
@@ -5,13 +6,14 @@ def readDraft(fname):
     f = open(fname, "r")
     lines = f.read().splitlines()
     f.close()
-    return lines
+    return parseDraft(lines)
 
 #Function to parse the draft and create a dict with relevant data
 def parseDraft(lines):
     #Some variables for keeping track of the topics and the dict object
     outlineDict = {
-        'title': ''
+        'title': '',
+        'data': {}
         }
     currentTopic = ''
     subtopic = ''
@@ -22,13 +24,13 @@ def parseDraft(lines):
             if line[0] == '#':
                 outlineDict['title'] = line[1:len(line)]
             elif line[0] == '>':
-                outlineDict[line[1:len(line)]] = {}
+                outlineDict['data'][line[1:len(line)]] = {}
                 currentTopic = line[1:len(line)]
             elif line[0] == '<':
-                outlineDict[currentTopic][line[1:len(line)]] = []
+                outlineDict['data'][currentTopic][line[1:len(line)]] = []
                 subtopic = line[1:len(line)]
             elif line[0] == '-':
-                outlineDict[currentTopic][subtopic].append(line[1:len(line)])
+                outlineDict['data'][currentTopic][subtopic].append(line[1:len(line)])
             #Something is wrong with the formatting, raise an exception
             else:
                 raise
